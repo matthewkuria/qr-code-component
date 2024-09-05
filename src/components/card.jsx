e4ee0cfd63e4acbbca48a27a77e7057e3056ba63
matthewkuria <mathewkwachira@gmail.com>
2024-09-05 13:54:08 +0300

    Set Font size 15px

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,15 +2,17 @@ import qrImage from "/image-qr-code.png"
 
 export default function Card() {
     return (
-        <div className=" p-4 bg-white rounded-lg h-[500px] w-[200px] flex flex-col ">
+        <div className=" p-2 bg-white rounded-lg h-[400px] w-[300px] flex flex-col ">
             <img src={qrImage}
                 alt="the qr-code image"
-                className="rounded-xl max-w-xs"
+                className="rounded-xl max-w-xs max-h-52"
                 width="auto"
-                height={30}
+                height={20}
             />
-            <h1>Improve your front-end skills by building projects</h1>
-            <p className="">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+            <div className="text-left p-4">
+                <h1 className="font-bold">Improve your front-end skills by building projects</h1>
+                <p className="">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+            </div>
         </div>
     )
 }
